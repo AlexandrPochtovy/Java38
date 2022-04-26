51c17c2e386d386b647cad09e150a9540f07219c
Alexandr Pochtovy <alex.mail.prime@gmail.com>
2022-04-26 19:44:51 +0300

    Сделал методы формирования и произвольного чтения массивов ArrayList и LinkedList

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,57 +2,78 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
-	// initial feature branch for Lesson8
+        // initial feature branch for Lesson8
+        int size = 1000000;
+        Date time1;
+        Date time2;
+        long deltaArray;//затраты времени на заполнение ArrayList с динамической памятью
+        long deltaList;//затраты времени на заполнение LinkedList
 
+        time1 = new Date();
+        ArrayList array1 = GenerateArrayList(size);
+        time2 = new Date();
+        deltaArray = time2.getTime() - time1.getTime();
+        System.out.println("Заполнение " + size + " елементов ArrayList занимает " + deltaArray + "мсек");
 
+        time1 = new Date();
+        LinkedList list1 = GenerateLinkedList(size);
+        time2 = new Date();
+        deltaList = time2.getTime() - time1.getTime();
+        System.out.println("Заполнение " + size + " елементов LinkedList занимает " + deltaList + "мсек");
+
+        time1 = new Date();
+        GetArrayList(array1);
+        time2 = new Date();
+        deltaArray = time2.getTime() - time1.getTime();
+        System.out.println("Произвольное чтение " + size + " елементов ArrayList занимает " + deltaArray + "мсек");
+
+        time1 = new Date();
+        GetArrayList(list1);
+        time2 = new Date();
+        deltaList = time2.getTime() - time1.getTime();
+        System.out.println("Произвольное чтение " + size + " елементов LinkedList занимает " + deltaList + "мсек");
     }
 
-    //генерирует массив ArrayList заданного размера заранее выделяя память
-    public static ArrayList<Integer> GenerateArrayListMem(int size) {
-        ArrayList<Integer> data = new ArrayList<>(size);
-        for (int i = 0; i < size; i++) {
-            data.set(i, (int)(Math.random() * size));
-        }
-        return data;
-    }
     //генерирует массив ArrayList заданного размера выделяя память динамически
-    public static ArrayList<Integer> GenerateArrayListNew(int size) {
-        ArrayList<Integer> data = new ArrayList<>();
+    public static ArrayList<Integer> GenerateArrayList(int size) {
+        ArrayList<Integer> data = new ArrayList<Integer>();
         for (int i = 0; i < size; i++) {
-            data.add((int)(Math.random() * size));
+            data.add((int) (Math.random() * size));
         }
         return data;
     }
 
     //генерирует массив LinkedList заданного размера выделяя память динамически
-    public static LinkedList<Integer> GenerateLinkedListNew(int size) {
+    public static LinkedList<Integer> GenerateLinkedList(int size) {
         LinkedList<Integer> data = new LinkedList<Integer>();
         for (int i = 0; i < size; i++) {
-            data.add((int)(Math.random() * size));
+            data.add((int) (Math.random() * size));
         }
         return data;
     }
 
     //чтение элемента ArrayList
-    public static void GerArrayList(ArrayList<Integer> data) {
+    public static void GetArrayList(ArrayList<Integer> data) {
         int len = data.size();
         int index;
         for (int i = 0; i < len; i++) {
-            index = Math.max((int)(Math.random() * len), len);
-            data.get(index);
+            index = Math.max((int) (Math.random() * len - 1), len - 1);
+            int temp = data.get(index);
         }
     }
+
     //чтение элемента LinkedList
-    public static void GerArrayList(LinkedList<Integer> data) {
+    public static void GetArrayList(LinkedList<Integer> data) {
         int len = data.size();
         int index;
         for (int i = 0; i < len; i++) {
-            index = Math.max((int)(Math.random() * len), len);
-            data.get(index);
+            index = Math.max((int) (Math.random() * len - 1), len - 1);
+            int temp = data.get(index);
         }
     }
 }
