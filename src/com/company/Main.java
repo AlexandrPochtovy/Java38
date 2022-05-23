9c18723e7cf2ecc14e7f685fa3f61cec5b03b413
Alexandr Pochtovy <alex.mail.prime@gmail.com>
2022-05-23 19:54:05 +0300

    тестовое задание на произведение двух чисел защита от ввода мусора

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,68 @@
 package com.company;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// initial main branch
+	/*
+	* в цикле программа просит у пользователя два числа, выводит произведение
+	* после спрашивает надо ли выйти, если нажато 1 то выход, продолжение - любая другая клавиша
+	* сделал также защиту от ввода мусора и повторный запрос ввода на каждую переменную
+    */
+        String strA = new String("Enter value a ");
+        String strB = new String("Enter value b ");
+        String strRes = new String("Multiplication result = ");
+        String mask = new String("1");
+        String req = new String("For exit press " + mask + ", for repeat press any key ");
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        String exitCom;
+    Scanner sc = new Scanner(System.in);
+    do {
+        int step = 1;
+        do {
+            try {
+                System.out.print(strA);
+                a = sc.nextInt();
+                step = 0;
+            } catch (InputMismatchException e) {
+                System.out.println("Enter NUMBERS!!!");
+                sc.nextLine();
+            }
+        } while (step == 1);
+
+        step = 1;
+        do {
+            try {
+                System.out.print(strB);
+                b = sc.nextInt();
+                step = 0;
+            } catch (InputMismatchException e) {
+                System.out.println("Enter NUMBERS!!!");
+                sc.nextLine();
+            }
+        } while (step == 1);
+
+        /*System.out.print(strB);
+        try {
+            b = sc.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Enter NUMBERS!!!");
+            sc.nextLine();
+        }
+        finally {
+            b = 1;
+        }*/
+        c = a * b;
+        System.out.print(strRes);
+        System.out.println(c);
+        System.out.print(req);
+        sc.nextLine();
+        exitCom = sc.nextLine();
+    } while (exitCom.equals(mask) == false);
+        sc.close();
     }
 }
